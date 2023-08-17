a1f3756522d206699d62f2133376fe2482f6e973
Edgar Valentin Ruiz Padilla <edgarvrp22@gmail.com>
2023-08-17 08:44:31 -0600

    Update 2 Backend URI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
-const host = 'https://nocountry-production-78d6.up.railway.app';
-
+//const host = 'https://nocountry-production-78d6.up.railway.app';
+const host = 'http://140.84.160.208:8083';
 export const environment = {
     production: false,
     url: host,
