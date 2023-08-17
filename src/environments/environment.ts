328756e04e3df570fa18885f3b0df2c05e9cf972
Edgar Valentin Ruiz Padilla <edgarvrp22@gmail.com>
2023-08-17 08:32:03 -0600

    Update Endpoint Backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const host = 'https://nocountry-production-78d6.up.railway.app';
+//const host = 'https://nocountry-production-78d6.up.railway.app';
+
+const host = 'http://140.84.160.208:8083';
 
 export const environment = {
     production: false,
